1ef3653a7a5be9972024658e21a0d17dae0bf245
Jabran Rafique <hello@jabran.me>
2014-05-12 12:47:32 +0100

    update social icons, config, js

--- js/global-0.0.2.js
+++ js/global-0.0.2.js
@@ -1,4 +1,6 @@
-/*! Google Analytics */
+/*! Global JavaScipt functions | Jabran Rafique | 1.0.0 */
+
+// Google Analytics
 var _gaq = _gaq || [];
 _gaq.push(["_setAccount", "UA-9809111-15"]);
 _gaq.push(["_setDomainName", "jabran.me"]);
@@ -9,6 +11,9 @@ var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async
 ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";
 var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+// iPhone active state enabler script
+document.addEventListener("touchstart", function()	{}, false);
 /*! JavaScript Document / Copyright (c) 2014 http://jabran.me/sandbox/socialmediadotjs / MIT License / 1.4 */
 var facebook={fbinit:!1,init:function(a,b){window.onload=function(){var a=document.getElementById("fb-root");document.body.appendChild(a)};"undefined"!==typeof a?window.fbAsyncInit=function(){FB.init({appId:a.appid,channelUrl:a.channelurl,status:a.status||!1,cookie:!0,xfbml:!0,frictionlessRequests:a.flrequests||!1});facebook.fbinit=!0;document.getElementById("facebook-jssdk").src+="#xfbml=1&appid="+a.appid;"undefined"!==typeof a.width&&"undefined"!==typeof a.height?FB.Canvas.setSize({width:parseInt(a.width),
 height:parseInt(a.height)}):FB.Canvas.setSize();typeof("undefined"!==a.autogrow)?FB.Canvas.setAutoGrow(a.autogrow):FB.Canvas.setAutoGrow();"undefined"!==typeof a.x&&"undefined"!==typeof a.y&&FB.Canvas.scrollTo(parseInt(a.x),parseInt(a.y));b&&"function"===typeof b&&b.call(this,window.FB)}:this.fbinit=!0},share:function(a,b,c){this.fbinit&&FB.ui({method:"feed",name:a.title,link:a.url,picture:a.image,caption:a.caption,description:a.description},function(a){return a&&a.post_id?"function"===typeof b?b.call(this,
